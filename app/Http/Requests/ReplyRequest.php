e9109be5c1411460c08a395172a88db5520c6727
todd <hey_todd@foxmail.com>
2017-11-08 02:29:19 +0000

    notify

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -4,33 +4,12 @@ namespace App\Http\Requests;
 
 class ReplyRequest extends Request
 {
-    public function rules()
-    {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            };
-        }
-    }
+  public function rules()
+  {
+      return [
+          'content' => 'required|min:2',
+      ];
+  }
 
     public function messages()
     {
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -7,15 +7,29 @@ use App\Models\Reply;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
+use App\Notifications\TopicReplied;
+
 class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function updating(Reply $reply)
     {
         //
+        $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function created(Reply $reply)
+    {
+        $topic = $reply->topic;
+        $topic->increment('reply_count', 1);
+
+        // 如果评论的作者不是话题的作者，才需要通知
+        if ( ! $reply->user->isAuthorOf($topic)) {
+            $topic->user->notify(new TopicReplied($reply));
+        }
     }
 }
